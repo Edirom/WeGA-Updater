d083447839621a32d940ab54fc15b7259cd589cb
Peter Stadler <stadlerpeter@yahoo.fr>
2018-05-14 23:11:45 +0200

    setting params from Docker env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-cp ${WEGA_DATA_DIR}/docker.local.build.properties ${WEGA_DATA_DIR}/local.build.properties 
+# remove local.build.properties from data dir
+if [ -f  ${WEGA_DATA_DIR}/local.build.properties ] 
+then
+    rm ${WEGA_DATA_DIR}/local.build.properties
+fi
+
+# write parameters to local.build.properties
+# from Docker env variables 
+awk -v data_dir="${WEGA_DATA_DIR}" 'END { for (name in ENVIRON) {
+        if (name ~ /^UPDATER_/) print substr(name, 9)"="ENVIRON[name] >> (data_dir"/local.build.properties") ;
+    }
+} ' < /dev/null
+
+# start the webhook service
 exec /usr/local/bin/webhook -hooks=/var/opt/wega-updater/update-hook.json -verbose
